33b51603b5f2108584d0f039f26b5365dea23c22
Daoud Aslan <daoudaslan@gmail.com>
2025-05-23 09:20:39 -0400

    Update NavBar.jsx

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,11 +1,11 @@
 export default function NavBar() {
     return (
-        <nav className="flex justify-between items-center h-20 bg-amber-100 text-black px-4">
+        <nav className="flex justify-between items-center h-20 bg-amber-100">
             <ul className="space-x-4 hidden sm:flex">
-                <li>Accueil</li>
-                <li>Destinations</li>
-                <li>Circuits</li>
-                <li>Contact</li>
+                <li> <a href="/">Accueil</a></li>
+                <li> <a href="/destinations">Destinations</a></li>
+                <li> <a href="/circuits">Circuits</a></li>
+                <li> <a href="/contact">Contact</a></li>
             </ul>
         </nav>
     );
