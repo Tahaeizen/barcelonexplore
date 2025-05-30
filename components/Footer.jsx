7576d9598ecf08cb44d1ac65c5c5b0294a09f1bd
Ab-Taha <aitbella.mtaha@gmail.com>
2025-05-30 11:54:35 -0400

    Finalisation semaine 3

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <footer className="bg-amber-100 text-black py-4 mt-16 font-sans border-t border-amber-200">
-            <div className="max-w-6xl mx-auto px-4">
+        <footer className="bg-amber-100 text-black py-4 mt-6 font-sans border-t border-amber-200">
+            <div className="max-w-7xl mx-auto px-4 sm:px-8">
                 <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
                     {/* Colonne 1 */}
                     <div>
--- components/home/Info.jsx
+++ components/home/Info.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Info() {
     return (
         <section id="info" className="bg-amber-50 p-8 rounded-lg mb-8 scroll-mt-20">
-            <div className="max-w-4xl mx-auto">
+            <div className="max-w-3xl mx-auto">
                 <h2 className="text-3xl font-bold mb-8">À propos de nous</h2>
                 <p className="text-gray-600 mb-6">
                     BarcelonExplore est une agence de voyages spécialisée dans l'organisation d'événements et de séjours à Barcelone. 
